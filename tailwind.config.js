4d7a187eb43278f95da351671a8d2c58b168ff76
Joseph Riddle <joeriddles10@gmail.com>
2024-02-10 21:34:27 -0800

    Init project

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./content/**/*.html",
+    "./layouts/**/*.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
